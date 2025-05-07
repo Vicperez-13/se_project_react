f2c8cbc3f33cdb7fdbc5c23aecbf3023da0321a2
Vicperez_13 <vp5662021@gmail.com>
2025-05-07 15:15:01 -0700

    cards: add weather cards

--- src/components/Main/WeatherCard/WeatherCard.jsx
+++ src/components/Main/WeatherCard/WeatherCard.jsx
@@ -1,11 +1,36 @@
-import sunny from "../../../assets/sunny.png";
+import sunny from "../../../assets/day/clear.png";
 import "./WeatherCard.css";
+import {
+  weatherOptions,
+  defaultWeatherOptions,
+} from "../../../utils/constants";
 
-function WeatherCard() {
+function WeatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+      console.log("Weather Data:", weatherData),
+      option.day === weatherData.isDay &&
+        option.condition === weatherData.condition
+    );
+  });
+
+  let weatherOption;
+  if (filteredOptions.length === 0) {
+    weatherOption = defaultWeatherOptions[weatherData.isDay ? "day" : "night"];
+  } else {
+    weatherOption = filteredOptions[0];
+  }
+
   return (
     <section className="weather-card">
-      <p className="weather-card__temp"> 75 &deg; F</p>
-      <img src={sunny} alt="" className="weather-card__img" />
+      <p className="weather-card__temp"> {weatherData.temp.F} &deg; F</p>
+      <img
+        src={weatherOption?.url}
+        alt={`Card showing ${weatherData.option?.day ? "day" : "night"} time ${
+          weatherOption?.condition
+        } weather`}
+        className="weather-card__img"
+      />
     </section>
   );
 }
